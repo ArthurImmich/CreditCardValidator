071b181dcdee8bd71d3a780763ca13067fbaf276
ArthurImmich <arthurimmichof@gmail.com>
2021-07-02 23:26:35 -0700

    title changed

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CardValidator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
